74e7f1267780df8199bac011b127665cb313777c
mmkr7111 <mmkr7111@gmail.com>
2020-10-06 16:02:05 +0530

    [Manish Reddy]Add. Implementation of Annotation using Employee class

---
+++ Annotations/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Annotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Annotations in c#");
+            TestAnnotation.Display();
+        }
+    }
+}
